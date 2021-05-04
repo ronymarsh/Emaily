305bcaf711b79be4723bf419ebdc7b3173ce9550
ronymarsh <rony.marsh6@gmail.com>
2021-05-04 11:13:42 +0300

    added get started button

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Landing = () => {
+    const wrapper_css={
+        margin:'1% 5%',
+        textAlign:'center'
+    }
+    const startBtn_css={
+        backgroundColor:'#ff9999',
+        border:'none',
+        color:'white',
+        padding:'2% 5%',
+        cursor:'pointer',
+        fontSize:'16px'
+    }
     return (
-        <div style={{textAlign:'center'}}>
+        <div style={wrapper_css}>
             <h1>
                 Emaily!
             </h1>
-            Collect feedback from your users
+            <p>Collect feedback from your users</p>
+            <Link to='/surveys'>
+                <button style={startBtn_css}>GET STARTED</button>
+            </Link>
         </div>
     )
 }
